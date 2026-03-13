a94dd45c5ff6ce08437b77792cee614893bd038f
unknown <harshitmishra5348@gmail.com>
2026-03-13 15:11:59 +0530

    usecallback error

--- components/ScriptStoryWriterTool/ScriptStoryWriterTool.js
+++ components/ScriptStoryWriterTool/ScriptStoryWriterTool.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useMemo, useRef, useState } from 'react';
+import React, { useEffect, useMemo, useRef, useState, useCallback } from 'react';
 import { Tooltip } from 'react-tooltip';
 import { getAuthHeader } from '@/utils/auth';
 import ToggleButton from '../Form/ToggleButton';
